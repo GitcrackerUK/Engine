b81aaad4b2d70f5df704312143aa692cc7bb0860
Pawel <pawel.siwek@filament.uk.com>
2021-10-17 17:38:08 +0100

    added component ControlsSection

---
+++ src/components/card/ControlsSection.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ControlsSection extends Component {
+    render() {
+        return (
+            <div className='controlsWrapper'>
+                
+            </div>
+        )
+    }
+}
