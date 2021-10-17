b18c874deb0389a08d3f23617da9b18e39dd2183
Pawel <pawel.siwek@filament.uk.com>
2021-10-17 17:40:20 +0100

    added component EngineBay

---
+++ src/components/card/EngineBay/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../styles.scss'
+
+export default class EngineBay extends Component {
+    render() {
+        return (
+            <div className='engineBayWrapper'>
+                
+            </div>
+        )
+    }
+}
