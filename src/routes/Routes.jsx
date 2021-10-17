abe55b63eecf2eceb7aab12529e1229f4f28dcdf
Pawel <pawel.siwek@filament.uk.com>
2021-10-17 18:01:58 +0100

    routes

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,9 @@
+import {Router, Redirect, Route, Switch} from 'react-router-dom';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+export class Routes extends Components {
+    constructor(props){
+        super(props);
+    }
+}
