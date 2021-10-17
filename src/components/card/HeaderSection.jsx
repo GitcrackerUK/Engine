9342f7f8507076aaa61e0db8d9cf6646e6470007
Pawel <pawel.siwek@filament.uk.com>
2021-10-17 17:38:33 +0100

    added component HeaderSection

---
+++ src/components/card/HeaderSection.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HeaderSection extends Component {
+    render() {
+        return (
+            <div className='cardHeaderWrapper'>
+                <h1>R4</h1>
+            </div>
+        )
+    }
+}
