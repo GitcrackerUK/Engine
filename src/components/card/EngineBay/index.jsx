b5209764871260537b62878acaf175b57d03f453
Pawel <pawel.siwek@filament.uk.com>
2021-10-17 23:09:28 +0100

    imported Block

--- src/components/card/EngineBay/index.jsx
+++ src/components/card/EngineBay/index.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Block from './Block'
 import '../styles.scss'
 
 export default class EngineBay extends Component {
     render() {
         return (
             <div className='engineBayWrapper'>
-                
+                <Block></Block>
             </div>
         )
     }
