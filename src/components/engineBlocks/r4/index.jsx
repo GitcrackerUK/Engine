a302f43985d4ee3dd4ccd82e5610bc1d8477bc20
Pawel <pawel.siwek@filament.uk.com>
2021-10-17 17:40:37 +0100

    added component R4

---
+++ src/components/engineBlocks/r4/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Card from '../../card'
+import './styles.scss'
+
+export default class R4 extends Component {
+    
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div className='wrapper'>
+              <Card></Card>
+            </div>
+        );
+    }
+}
