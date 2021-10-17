59c113ac00653d4b22eee43112fedefb29f55ba6
Pawel <pawel.siwek@filament.uk.com>
2021-10-17 17:36:15 +0100

    removed Provider added scss and WrapperContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
+import WrapperContainer from './containers/WrapperContainer';
+import './app.scss'
 
 export default class App extends Component {
     render() {
         return (
-            <Provider store={store}>
-                <h1>Engine</h1>
-            </Provider>
+            <div className='main'>
+                <WrapperContainer>
+                </WrapperContainer>
+            </div>
         );
     }
 }
